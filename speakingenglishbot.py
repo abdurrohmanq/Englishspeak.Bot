669bc0bd064a179d0b40d73d5699e658d027f2e3
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-18 15:33:53 +0500

    EnglishSpeakBot

---
+++ speakingenglishbot.py
@@ -0,0 +1,36 @@
+import logging
+from aiogram import Bot,Dispatcher,executor,types
+
+from config import getdifinitions
+from googletrans import Translator
+translator=Translator()
+
+API_TOKEN='5736574870:AAEvV1ceO-ZJCdt_JZuwJFDI9SPYnZ7SoH0'
+
+bot=Bot(token=API_TOKEN)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(text="<b>Assalomu alaykum Varohmatullohi Vabarokatuh!!!</b>"+message.from_user.full_name+ " Dictionary botiga Hush kelibsiz",parse_mode='HTML')
+@dp.message_handler() 
+async def tarjimon(message:types.Message):
+    print(message)
+    lang=translator.detect(message.text).lang
+    if len(message.text.split())>2:
+        dest='uz' if lang=='en' else 'en'
+        await message.reply(translator.translate(message.text,dest).text)
+    else:
+        if lang=='en':
+            word_id=message.text
+        else:
+            word_id=translator.translate(message.text,dest='en').text
+        lookup=getdifinitions(word_id)
+        if lookup:
+            await message.reply(f"Word: {word_id} \nDefinitions: \n{lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Bunday so'z topilmadi")
+if __name__=='__main__':
+    executor.start_polling(dp,skip_updates=True)
